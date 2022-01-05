082cc1b526f70f9d8b3128051d7de18be10d19e3
waterleaper <waterleaper8@gmail.com>
2022-01-05 18:37:45 +0900

    Update README

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # これは何？
 
-わたしの別のリポジトリの django-horse-race-api と連携して作成した競馬予想の Web アプリケーションです。  
-完成品を以下にて公開しております。
-
-- [Horse Prediction - 競馬予想 AI](https://waterleaper.net/horse_prediction_react/)
+わたしの別のリポジトリの django-horse-race-api と連携して作成した競馬予想の Web アプリケーションです。
--- src/components/PredictApp.jsx
+++ src/components/PredictApp.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import FetchPredict from "./FetchPredict";
-import MotionSimple from "./MotionSimple";
-import { SMv, STitle } from "./Styled";
+import React from "react"
+import FetchPredict from "./FetchPredict"
+import MotionSimple from "./MotionSimple"
+import { SMv, STitle } from "./Styled"
 
 export const PredictApp = () => {
   return (
@@ -20,7 +20,7 @@ export const PredictApp = () => {
           <p>それぞれの馬が３着以内に入る確率を予測します。</p>
           <p>※馬体重の計測が完了しているレースのみ予測が可能です。</p>
           <p className="text-danger">
-            ※この予測モデルの回収率は80%程度です。
+            ※この予測モデルの回収率は90%程度です。
             <br />
             賭博による利益を保証するものではありませんので、ご了承ください。
           </p>
@@ -38,5 +38,5 @@ export const PredictApp = () => {
         </div>
       </div>
     </MotionSimple>
-  );
-};
+  )
+}
