0655e9e8ac68e650cec012fb7c0bfb1c44ebff20
waterleaper <waterleaper8@gmail.com>
2022-02-08 00:32:58 +0900

    Add opening animation

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
-import "./App.css";
-import { Switch, Route, useLocation } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
+import "./App.css"
+import { Switch, Route, useLocation } from "react-router-dom"
+import { AnimatePresence } from "framer-motion"
 
-import { Header } from "./components/Header";
-import { Home } from "./components/Home";
-import { Predict } from "./components/Predict";
-import { Odds } from "./components/Odds";
-import { Footer } from "./components/Footer";
-import { PredictApp } from "./components/PredictApp";
-import Loader from "./components/Loader";
-import { useState } from "react";
-import { useEffect } from "react";
-import { useTracking } from "./hooks/useTracking";
-import { OddsApp } from "./components/OddsApp";
+import { Header } from "./components/Header"
+import { Home } from "./components/Home"
+import { Predict } from "./components/Predict"
+import { Odds } from "./components/Odds"
+import { Footer } from "./components/Footer"
+import { PredictApp } from "./components/PredictApp"
+import Loader from "./components/Loader"
+import { useState } from "react"
+import { useEffect } from "react"
+import { useTracking } from "./hooks/useTracking"
+import { OddsApp } from "./components/OddsApp"
 
 function App() {
-  useTracking("G-0NGNESXE8K");
+  useTracking("G-0NGNESXE8K")
 
-  const location = useLocation();
-  const [loaded, setLoaded] = useState(false);
+  const location = useLocation()
+  const [loaded, setLoaded] = useState(false)
   function disableScroll(event) {
-    event.preventDefault();
+    event.preventDefault()
   }
   useEffect(() => {
-    document.body.classList.add("hide");
-    document.addEventListener("touchmove", disableScroll, { passive: false });
+    document.body.classList.add("hide")
+    document.addEventListener("touchmove", disableScroll, { passive: false })
     setTimeout(() => {
-      setLoaded(true);
-      document.body.classList.remove("hide");
+      setLoaded(true)
       document.removeEventListener("touchmove", disableScroll, {
         passive: false,
-      });
-    }, 3000);
-  }, []);
+      })
+    }, 3000)
+  }, [])
 
   return (
     <>
-      <Loader className={loaded ? "fadeOut" : ""} />
+      <Loader className={loaded ? "fadeOut" : ""} loaded={loaded} />
+      {/* <Loader loaded={loaded} /> */}
       <div className={"container-lg"}>
         <Header />
         <AnimatePresence exitBeforeEnter initial={false}>
@@ -61,7 +61,7 @@ function App() {
         <Footer />
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,15 +1,105 @@
-import React from "react";
+import React, { useEffect } from "react"
+import gsap from "gsap"
 
 const Loader = (props) => {
+  const loaded = props.loaded
+  const tween = gsap.timeline({})
+  tween.fromTo(
+    ".loader",
+    {
+      opacity: 1,
+    },
+    {
+      opacity: 0,
+      display: "none",
+      ease: "Power4.out",
+    }
+  )
+  tween.fromTo(
+    ".top_title",
+    {
+      display: "block",
+      opacity: 0,
+      scale: 0.2,
+    },
+    {
+      display: "block",
+      opacity: 1,
+      scale: 1,
+      ease: "Power4.out",
+    }
+  )
+  tween.to(".top_title_innerLeft", {
+    x: -1000,
+    delay: 0.3,
+  })
+  tween.to(
+    ".top_title_innerRight",
+    {
+      x: 1000,
+    },
+    "<"
+  )
+  tween.to("#loader", {
+    opacity: 0,
+    display: "none",
+  })
+
+  useEffect(() => {
+    console.log(loaded)
+    if (loaded) {
+      tween.play()
+      setTimeout(() => {
+        document.body.classList.remove("hide")
+      }, 2000)
+    } // eslint-disable-next-line
+  }, [loaded])
+
   return (
+    // <div
+    //   id="loader"
+    //   className={`d-flex align-items-center w-100 vh-100 position-fixed top-0 left-0 bg-dark ${props.className}`}
+    // >
     <div
       id="loader"
-      className={`d-flex align-items-center w-100 vh-100 position-fixed top-0 left-0 bg-dark ${props.className}`}
+      className={`align-items-center w-100 h-100 position-fixed top-0 left-0 bg-dark`}
     >
-      <div className="loader">Loading...</div>
+      <div className="loader"></div>
+      <div className="top_title">
+        <span className="top_title_innerLeft">Horse</span>
+        <span className="top_title_innerRight">Prediction</span>
+      </div>
+
       <style jsx="true">{`
+        .top_title {
+          position: fixed;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          -webkit- transform: translateY(-50%) translateX(-50%);
+          display: none;
+          font-size: 9rem;
+          color: #fff;
+          font-family: "EB Garamond", serif;
+        }
+        @media screen and ( max-width : 1023px ) {
+          .top_title {
+            font-size: 7.2rem;
+          }
+        }
+        @media screen and ( max-width : 767px ) {
+          .top_title {
+            font-size: 4.2rem;
+          }
+        }
+        .top_title_innerLeft,
+        .top_title_innerRight {
+          display: inline-block;
+        }
         #loader {
           z-index: 9999;
+          display: flex;
+          justify-content: center;
         }
         .fadeOut {
           opacity: 0;
@@ -28,7 +118,6 @@ const Loader = (props) => {
         .loader {
           color: #ffffff;
           text-indent: -9999em;
-          margin: 88px auto;
           position: relative;
           font-size: 11px;
           -webkit-transform: translateZ(0);
@@ -77,7 +166,7 @@ const Loader = (props) => {
         }
       `}</style>
     </div>
-  );
-};
+  )
+}
 
-export default Loader;
+export default Loader
